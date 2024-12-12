902c92d37606458e13c4cf9374c62b1fe9e020ed
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2024-12-12 14:22:47 -0400

    manejando variables por parametros

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
-
+import zoneinfo
+from datetime import datetime
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+country_timezones = {
+    "US": "America/New_York",
+    "UK": "Europe/London",
+    "DE": "Europe/Berlin",
+    "FR": "Europe/Paris",
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+}
+
+@app.get("/time/{iso_code}")
+async def time(iso_code:str):
+    iso=iso_code.upper()
+    timezone_str=country_timezones.get(iso)
+    tz=zoneinfo.ZoneInfo(timezone_str)
+    return {"time":datetime.now(tz)}
