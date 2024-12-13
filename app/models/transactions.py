eab119060d9a5c53b846b449d12244f5e409293f
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2024-12-13 14:14:16 -0400

    crud para tranasactions

--- app/models/transactions.py
+++ app/models/transactions.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 
-
+class TransactionBase(SQLModel):
+    amount:int =Field(default=None)
+    description: str =Field(default=None)
+  
+class TransactionCreate(TransactionBase):
+    pass
+class TransactionUpdate(TransactionBase):
+    pass  
     
-class Transaction(BaseModel):
-    id:int
-    amount:int
-    description: str
+class Transaction(TransactionBase, table=True):
+    id:int | None = Field(default=None, primary_key=True)
--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -1,9 +1,51 @@
 from fastapi import APIRouter,HTTPException,status 
-from ..models.transactions import Transaction
+from ..models.transactions import Transaction,TransactionCreate,TransactionUpdate
 from db import SessionDep
 from sqlmodel import select
 
 router = APIRouter()
-@router.post("/transactions" ,tags=['transactions'])
-async def create_transaction(transaction_data:Transaction):
-    return transaction_data
+@router.get("/transactions",response_model=list[Transaction] ,tags=['transactions'])
+async def get_transactions(session:SessionDep):
+    return session.exec(select(Transaction)).all() 
+
+@router.get("/transactions/{transaction_id}", response_model=Transaction, tags=["transactions"])
+async def read_transaction(transaction_id: int, session: SessionDep):
+    transaction_db = session.get(Transaction, transaction_id)
+    if not transaction_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Transaction doesn't exist"
+        )
+    return transaction_db
+
+@router.post("/transactions", response_model=Transaction, tags=["transactions"], status_code=status.HTTP_201_CREATED)
+async def create_transaction(transaction_data: TransactionCreate, session: SessionDep):
+    transaction = Transaction.model_validate(transaction_data.model_dump())
+    session.add(transaction)
+    session.commit()
+    session.refresh(transaction)
+    return transaction
+
+@router.patch("/transactions/{transaction_id}", response_model=Transaction, tags=["transactions"], status_code=status.HTTP_200_OK)
+async def edit_transaction(transaction_id: int, transaction_data: TransactionUpdate, session: SessionDep):
+    transaction_db = session.get(Transaction, transaction_id)
+    if not transaction_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Transaction doesn't exist"
+        )
+    update_data = transaction_data.model_dump(exclude_unset=True)
+    transaction_db.sqlmodel_update(update_data)
+    session.add(transaction_db)
+    session.commit()
+    session.refresh(transaction_db)
+    return transaction_db
+
+@router.delete("/transactions/{transaction_id}", tags=["transactions"])
+async def delete_transaction(transaction_id: int, session: SessionDep):
+    transaction_db = session.get(Transaction, transaction_id)
+    if not transaction_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Transaction doesn't exist"
+        )
+    session.delete(transaction_db)
+    session.commit()
+    return {"detail": "Deleted transaction"}
