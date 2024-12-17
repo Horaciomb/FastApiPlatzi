3e49c27762b7308a409dd7007ddc6f3fd60a5e24
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2024-12-17 10:19:37 -0400

    autenticación senscilla

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Request 
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, Request ,status
 import time
 import zoneinfo
 from datetime import datetime
+from fastapi.security import  HTTPBasic, HTTPBasicCredentials
 from db import create_all_tables
 from .routers.customers import router as customer_router
 from .routers.transactions import router as transaction_router
@@ -24,19 +26,24 @@ async def log_request_time(request: Request, call_next):
     procces_time=time.time() - start_time
     print(f"Request: {request.url} completed in: {procces_time:.4f} seconds")
     return response
-@app.middleware("http") 
-async def log_request_headers(request: Request, call_next):
+# @app.middleware("http") 
+# async def log_request_headers(request: Request, call_next):
     
-    print("Request Headers:")
-    for header, value in request.headers.items():
-        print(f"{header}: {value}")
+#     print("Request Headers:")
+#     for header, value in request.headers.items():
+#         print(f"{header}: {value}")
 
-    response = await call_next(request) 
+#     response = await call_next(request) 
 
-    return response
+#     return response
+security=HTTPBasic()
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(credentials: Annotated[HTTPBasicCredentials,Depends(security)]):
+    print(credentials)
+    if credentials.username == "horacio" and credentials.password=="1234":
+        return {"message": f"Hello, {credentials.username}"}
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
 country_timezones = {
     "US": "America/New_York",
